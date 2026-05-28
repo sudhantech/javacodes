49326a44ccec78308448398f43cb52a5023a6309
sudhantech <sdhrsanm@gmail.com>
2026-05-28 17:23:16 +0530

    First commit

---
+++ nc1.java
@@ -0,0 +1,7 @@
+public class nc1{
+
+    public static void main(String [] args){
+      String name = "sudharsan";
+      System.out.println("This is my first code in java form visula studio code editor to use , My name is "+name);
+    }
+}
