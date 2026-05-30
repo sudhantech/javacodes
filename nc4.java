3be3cc07800afd54fd657df0ebf45d62b3628603
sudhantech <sdhrsanm@gmail.com>
2026-05-30 20:51:17 +0530

    class and object concept

---
+++ nc4.java
@@ -0,0 +1,14 @@
+class student{
+  String name="george";
+
+  void display(){
+    System.out.println("Name :"+name);
+  }
+}
+
+public class nc4{
+  public static void main(String []args){
+    student s=new student();
+    s.display();
+  }
+}
