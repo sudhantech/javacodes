044365880943ca86fbf27a7c883ef95f909777d0
sudhantech <sdhrsanm@gmail.com>
2026-07-25 20:35:10 +0530

    This program is compile time polymorphism

---
+++ CompiletimePolymorphism.java
@@ -0,0 +1,19 @@
+class Calculator{
+  int add(int a, int b){
+    return a+b;
+  }
+  int add(int a,int b,int c){
+    return a+b+c;
+  }
+  double add(double a,double b){
+    return a+b;
+  }
+}
+public class CompiletimePolymorphism {
+  public static void main(String args[]){
+    Calculator c=new Calculator();
+    System.out.println(c.add(10,20));
+    System.out.println(c.add(30,20));
+    System.out.println(c.add(10.0,20.0));
+  }
+}
